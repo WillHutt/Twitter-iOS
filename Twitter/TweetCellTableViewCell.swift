75806c038134f56bae299c59c72e60e374098173
William Hutt <whutt1994@gmail.com>
2019-02-11 18:10:52 -0800

    First part of Twitter iOS program

---
+++ Twitter/TweetCellTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  TweetCellTableViewCell.swift
+//  Twitter
+//
+//  Created by William Hutt on 2/11/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCellTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var profileNameLabel: UILabel!
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
